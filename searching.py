4ff2e33e7333fc1cb885989f1ea29a3cbe9c001d
RadikSedlak <radovan.sedlaak@gmail.com>
2026-04-13 13:31:12 +0200

    druha zmena finalni

--- searching.py
+++ searching.py
@@ -2,20 +2,37 @@ from pathlib import Path
 import json
 
 def read_data(filename, field):
-    with open(filename, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    if field not in data:
+    f = open(filename, "r", encoding="utf-8")
+    data = json.load(f)
+    f.close()
+
+    if field in data:
+        return data[field]
+    else:
         return None
-    return data[field]
+
+
+
+
+def linear_search(sequence, target):
+    positions = []
+    i = 0
+
+    for item in sequence:
+        if item == target:
+            positions.append(i)
+        i = i + 1
+
+    return {"positions": positions, "count": len(positions)}
 
 
 def main():
-    sequential_data = read_data("sequential.json", "unordered_numbers")
-    print(sequential_data)
+    data = read_data("sequential.json", "unordered_numbers")
+    target = 5
+
+    result = linear_search(data, target)
+    print(result)
 
 
 if __name__ == "__main__":
     main()
-
-
-
